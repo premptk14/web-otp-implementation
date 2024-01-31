060759cb0725312e5cb0ce80e0e3ba7d06cbdceb
Prem R Pattanayak <prem.p.in@rupeek.com>
2024-01-31 14:06:55 +0530

    adding web otp api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [otp, setOtp] = useState();
+
+  useEffect(()=>{
+    const ac = new AbortController();
+    if ('OTPCredential' in window) {
+        const input = document.querySelector('input[autocomplete="one-time-code"]');
+        if (!input) return;
+        const form = input.closest('form');
+        if (form) {
+          form.addEventListener('submit', e => {
+            ac.abort();
+          });
+        }
+        navigator.credentials.get({
+          otp: { transport:['sms'] },
+          signal: ac.signal
+        }).then(otp => {
+          input.value = otp.code;
+          setOtp(otp.code);
+          // if (form) form.submit();
+        }).catch(err => {
+          console.log(err);
+        });
+    }
+    
+    return ()=>{ac.abort();};
+  },[])
+
+    return (
+      <div className="App">
+        
+        <h2>Your OTP is: {otp}</h2>
+        <form>
+          <input autocomplete="one-time-code" required/>
+          <input type="submit"/>
+        </form>
+
+      </div>
+    );
+  }
 
 export default App;
